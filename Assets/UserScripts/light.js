fcaaf3e34bab91c8e8e0b6dd9cc19c324637174b
Duncan Fairbanks <bonsairobo@gmail.com>
2015-08-15 05:07:16 +0000

    clang format, remove statics, remove mp3s

--- Assets/UserScripts/light.js
+++ Assets/UserScripts/light.js
@@ -1,46 +1,42 @@
 // dynamic lighting effects
 
-static var THRESH = 3.0;
-static var DELAY = 1.0;
-static var LIGHT_SCALE = 0.06;
+var THRESH = 3.0;
+var DELAY = 1.0;
+var LIGHT_SCALE = 0.06;
 
-static var color_idx = 0;
-static var counter = 0.0;
-static var prev_amp_sum = 0.0;
+var color_idx = 0;
+var counter = 0.0;
+var prev_amp_sum = 0.0;
 
-static var color_cycle: Color[];
+var color_cycle : Color[];
 
-function Start()
-{
-	color_cycle = new Color[5];
+function Start() {
+  color_cycle = new Color[5];
 
-	color_cycle[0] = Color.white;
-	color_cycle[1] = Color.red;
-	color_cycle[2] = Color.yellow;
-	color_cycle[3] = Color.blue;
-	color_cycle[4] = Color.magenta;
+  color_cycle[0] = Color.white;
+  color_cycle[1] = Color.red;
+  color_cycle[2] = Color.yellow;
+  color_cycle[3] = Color.blue;
+  color_cycle[4] = Color.magenta;
 }
 
-function Update()
-{
-	var amp_sum = 0.0;
-	for (var i = 0; i < spectrum.note_amps.length; ++i)
-	{
-		amp_sum += spectrum.note_amps[i];
-	}
+function Update() {
+  var amp_sum = 0.0;
+  for (var i = 0; i < spectrum.note_amps.length; ++i) {
+    amp_sum += spectrum.note_amps[i];
+  }
 
-	counter += Time.deltaTime;
+  counter += Time.deltaTime;
 
-	if ((amp_sum - prev_amp_sum) > THRESH && counter > DELAY)
-	{
-		++color_idx;
-		if (color_idx == 5) color_idx = 0;
-		light.color = color_cycle[color_idx];
-		counter = 0.0;
-	}
+  if ((amp_sum - prev_amp_sum) > THRESH && counter > DELAY) {
+    ++color_idx;
+    if (color_idx == 5) color_idx = 0;
+    light.color = color_cycle[color_idx];
+    counter = 0.0;
+  }
 
-	prev_amplitude = amp_sum;
-	light.intensity = LIGHT_SCALE * amp_sum;
+  prev_amplitude = amp_sum;
+  light.intensity = LIGHT_SCALE * amp_sum;
 
-	Debug.Log(light.intensity);
+  Debug.Log(light.intensity);
 }
--- Assets/UserScripts/spectrum.js
+++ Assets/UserScripts/spectrum.js
@@ -1,54 +1,48 @@
 // sorts linear spectrum into harmonic model of ~5 octaves
 
-static var TWO_TWELFTH = Mathf.Pow(2, 1.0 / 12);
-static var NUM_BINS = 8192;
-static var MAX_FREQ: float = AudioSettings.outputSampleRate / 2;
-static var BIN_SIZE = MAX_FREQ / NUM_BINS;
-static var MIDDLE_A = 440;
-static var NUM_NOTES = 108;
-static var MIDDLE_A_IDX = 40;
+var TWO_TWELFTH = Mathf.Pow(2, 1.0 / 12);
+var NUM_BINS = 8192;
+var MAX_FREQ : float = AudioSettings.outputSampleRate / 2;
+var BIN_SIZE = MAX_FREQ / NUM_BINS;
+var MIDDLE_A = 440;
+var NUM_NOTES = 108;
+var MIDDLE_A_IDX = 40;
 
-static var bin_idx = 0;
+var bin_idx = 0;
 
-static var bins: float[];
-static var note_amps: float[];
-static var note_freqs: float[];
+var bins : float[];
+var note_amps : float[];
+var note_freqs : float[];
 
-static var min_note_bin = 0;
-static var max_note_bin = 0;
+var min_note_bin = 0;
+var max_note_bin = 0;
 
-function Start()
-{
-	bins = new float[NUM_BINS];
-	note_amps = new float[NUM_NOTES];
-	note_freqs = new float[NUM_NOTES + 1];
+function Start() {
+  bins = new float[NUM_BINS];
+  note_amps = new float[NUM_NOTES];
+  note_freqs = new float[NUM_NOTES + 1];
 
-	for (var i = 0; i < NUM_NOTES + 1; ++i)
-	{
-		note_freqs[i] = MIDDLE_A * Mathf.Pow(TWO_TWELFTH, i - MIDDLE_A_IDX);
-	}
+  for (var i = 0; i < NUM_NOTES + 1; ++i) {
+    note_freqs[i] = MIDDLE_A * Mathf.Pow(TWO_TWELFTH, i - MIDDLE_A_IDX);
+  }
 
-	min_note_bin = Mathf.Floor(note_freqs[0] * NUM_BINS / MAX_FREQ);
-	max_note_bin = Mathf.Floor(note_freqs[NUM_NOTES] * NUM_BINS / MAX_FREQ);
+  min_note_bin = Mathf.Floor(note_freqs[0] * NUM_BINS / MAX_FREQ);
+  max_note_bin = Mathf.Floor(note_freqs[NUM_NOTES] * NUM_BINS / MAX_FREQ);
 }
 
-function Update()
-{
-	for (var i = 0; i < note_amps.length; ++i)
-	{
-		note_amps[i] = 0;
-	}
+function Update() {
+  for (var i = 0; i < note_amps.length; ++i) {
+    note_amps[i] = 0;
+  }
 
-	AudioListener.GetSpectrumData(bins, 0, FFTWindow.Blackman);
+  AudioListener.GetSpectrumData(bins, 0, FFTWindow.Blackman);
 
-	var note_idx = 0;
-	for (var j = min_note_bin; j <= max_note_bin; ++j)
-	{
-		while (j * MAX_FREQ / NUM_BINS >= note_freqs[note_idx + 1])
-		{
-			++note_idx;
-		}
+  var note_idx = 0;
+  for (var j = min_note_bin; j <= max_note_bin; ++j) {
+    while (j * MAX_FREQ / NUM_BINS >= note_freqs[note_idx + 1]) {
+      ++note_idx;
+    }
 
-		note_amps[note_idx] += bins[j];
-	}
+    note_amps[note_idx] += bins[j];
+  }
 }
